d1051bca2ea0d00d5e046bdc5fef1fc295647ab8
Anneloes <anneloes9405@hotmail.com>
2020-02-09 01:20:36 +0000

    made Users queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -30,16 +30,6 @@ const getUserWithEmail = function(email) {
   WHERE email = $1
   `, [email])
   .then(res => res.rows || null)
-  // let user;
-  // for (const userId in users) {
-  //   user = users[userId];
-  //   if (user.email.toLowerCase() === email.toLowerCase()) {
-  //     break;
-  //   } else {
-  //     user = null;
-  //   }
-  // }
-  // return Promise.resolve(user);
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -49,7 +39,12 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool.query(`
+  SELECT name
+  FROM users
+  WHERE id = $1
+  `, [id])
+  .then(res => res.rows || null)
 }
 exports.getUserWithId = getUserWithId;
 
@@ -60,10 +55,13 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  const {name, email, password} = user
+  return pool.query(`
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *
+  `, [name, email, password])
+  .then(res => res.rows)
 }
 exports.addUser = addUser;
 
