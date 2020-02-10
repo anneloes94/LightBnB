4479d11de0092431fbd7d84f3f7206ae2181ea28
Anneloes <anneloes9405@hotmail.com>
2020-02-10 00:39:12 +0000

    added getAllReservations, not tested

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -73,7 +73,18 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT reservations.*, properties.*, avg(property_reviews.rating)
+    FROM reservations
+    JOIN properties on reservations.property_id = properties.id
+    JOIN property_reviews on property_reviews.property_id = properties.id
+    WHERE end_date < now()::date
+    AND reservations.guest_id = $1
+    GROUP BY reservations.id, properties.id
+    ORDER BY start_date
+    LIMIT $2;`
+  , [guest_id, limit])
+  .then(res => res.rows);
 }
 exports.getAllReservations = getAllReservations;
 
